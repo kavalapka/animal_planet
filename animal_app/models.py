3cef73fa3f6452e1bcadccdd433f4d0501172489
Katerina Zenchik <e.zenchik@gmail.com>
2018-03-16 14:08:18 +0300

    add migrations

--- animal_app/models.py
+++ animal_app/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Animal(models.Model):
-    pass
+    name =  models.CharField(max_length=100)
+    total_amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '[{}] {}'.format(self.id, self.name)
