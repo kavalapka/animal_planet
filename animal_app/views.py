c93619f852f477522ebae49df9c741cc06785863
Katerina Zenchik <e.zenchik@gmail.com>
2018-03-16 17:32:26 +0300

    +views and templates

--- animal_app/views.py
+++ animal_app/views.py
@@ -1,9 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
 
 from animal_app.models import Animal
 
 
 def index(request):
-    Animal
-    return HttpResponse("Hello, world. You're at the Animals index.")
+    latest_animal_list = Animal.objects.order_by('-id')[:5]
+    template = loader.get_template('animals/index.html')
 
+    context = {'latest_animal_list': latest_animal_list}
+    return HttpResponse(template.render(context))
+
+class based_view:
+    pass
+
+def detail(request, animal_id):
+    try:
+        animal = Animal.objects.get(pk=animal_id)
+    except Animal.DoesNotExist:
+        raise Http404("Animal does not exist")
+    return render(request, 'animals/detail.html', {'animal': animal})
