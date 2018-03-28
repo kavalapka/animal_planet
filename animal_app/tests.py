db7bc64434cf61a5b5563b2b6c132415461d4374
Katerina Zenchik <e.zenchik@gmail.com>
2018-03-28 16:14:48 +0300

    add tests to AnimalViewSet

--- animal_app/tests.py
+++ animal_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from animal_app.models import Animal
 
-# Create your tests here.
+
+class AnimalViewTests(TestCase):
+    def test_animal_list_view(self):
+        response = self.client.get('/animal/')
+        self.assertEqual(response.status_code, 200)
+
+
+class AnimalDetailView(TestCase):
+    def test_animal_detail_view(self):
+        new_animal = Animal.objects.create(name='test', number='1', owner_id='1')
+        response = self.client.get('/animal/{}/'.format(new_animal.id))
+        self.assertEqual(response.status_code, 200)
