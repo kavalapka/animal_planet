6be07dc7882fe21a23abf3633d37a32571e16685
Katerina Zenchik <e.zenchik@gmail.com>
2018-03-29 18:14:28 +0300

    add selenium test

--- animal_app/tests.py
+++ animal_app/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import LiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
 from animal_app.models import Animal
 
 
@@ -13,3 +15,24 @@ class AnimalDetailView(TestCase):
         new_animal = Animal.objects.create(name='test', number='1', owner_id='1')
         response = self.client.get('/animal/{}/'.format(new_animal.id))
         self.assertEqual(response.status_code, 200)
+
+
+class UsersSeleniumTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(UsersSeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(UsersSeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('http://127.0.0.1:8000/admin/')
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('test')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('test')
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+        red = self.client.get(self.selenium.current_url)
+        self.assertEqual(red.status_code, 302)
